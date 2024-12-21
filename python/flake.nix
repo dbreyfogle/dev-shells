7070b96529b82c5a48052e8e4b811f3a84b9981b
Danny Breyfogle <dwbreyfogle@gmail.com>
2024-12-21 14:09:48 -0700

    init: starter dev shells

---
+++ python/flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages =
+            with pkgs;
+            [ python311 ]
+            ++ (with pkgs.python311Packages; [
+              pip
+              venvShellHook
+            ]);
+          venvDir = ".venv";
+        };
+      }
+    );
+}
